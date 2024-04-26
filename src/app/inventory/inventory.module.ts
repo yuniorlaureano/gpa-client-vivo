c8c05c6a2a24a00b7354bf8e9972bf4c9de504db
oamroinuy@outlook.com <oamroinuy@outlook.com>
2024-04-26 13:46:01 -0400

    adding ng-bootstrap

--- src/app/inventory/inventory.module.ts
+++ src/app/inventory/inventory.module.ts
@@ -5,9 +5,20 @@ import { InventoryRoutingModule } from './inventory-routing.module';
 import { ProductComponent } from './product/product.component';
 import { CategoryComponent } from './category/category.component';
 import { ManufacturedProductEntryComponent } from './manufactured-product-entry/manufactured-product-entry.component';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ProductComponent, CategoryComponent, ManufacturedProductEntryComponent],
-  imports: [CommonModule, InventoryRoutingModule],
+  declarations: [
+    ProductComponent,
+    CategoryComponent,
+    ManufacturedProductEntryComponent,
+  ],
+  imports: [
+    CommonModule,
+    InventoryRoutingModule,
+    NgbDatepickerModule,
+    FormsModule,
+  ],
 })
 export class InventoryModule {}
