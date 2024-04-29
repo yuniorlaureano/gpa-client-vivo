8ff8192da7297412a0818c4bd015b7dd9f18195e
oamroinuy@outlook.com <oamroinuy@outlook.com>
2024-04-29 17:17:55 -0400

    Add sele escreen

--- src/app/invoice/invoice.module.ts
+++ src/app/invoice/invoice.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { InvoiceRoutingModule } from './invoice-routing.module';
 import { SaleComponent } from './sale/sale.component';
-import { RouterModule } from '@angular/router';
 import { PurchaseComponent } from './purchase/purchase.component';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SaleComponent, PurchaseComponent],
-  imports: [CommonModule, InvoiceRoutingModule],
+  imports: [
+    CommonModule,
+    InvoiceRoutingModule,
+    NgbDatepickerModule,
+    FormsModule,
+  ],
 })
 export class InvoiceModule {}
--- src/app/invoice/sale/sale.component.ts
+++ src/app/invoice/sale/sale.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'gpa-sale',
   templateUrl: './sale.component.html',
-  styleUrl: './sale.component.css'
+  styleUrl: './sale.component.css',
 })
 export class SaleComponent {
+  isProductCatalogVisible: boolean = false;
+  isClientCatalogVisible: boolean = false;
 
+  handleShowProductCatalog(visible: boolean) {
+    this.isProductCatalogVisible = visible;
+  }
+
+  handleShowClientCatalog(visible: boolean) {
+    this.isClientCatalogVisible = visible;
+  }
 }
