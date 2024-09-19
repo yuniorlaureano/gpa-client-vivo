f1c1a61de3ea3497dd213ffce1c9b30632d72594
YLaureano@dacgroup.com <YLaureano@dacgroup.com>
2024-09-19 09:28:17 -0400

    fix loading reasons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngxs/store';
 import {
-  LoadReasons,
   MapLoaded,
   SetCurrentMenu,
   SetCurrentSubMenu,
 } from './core/ng-xs-store/actions/app.actions';
 import { AppState } from './core/ng-xs-store/states/app.state';
 import { ToastService } from './core/service/toast.service';
-import { FilterModel } from './core/models/filter.model';
-import { ReasonService } from './inventory/service/reason.service';
 import { Subscription } from 'rxjs';
-import { processError } from './core/utils/error.utils';
-import { ErrorService } from './core/service/error.service';
+
 @Component({
   selector: 'gpa-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'gpa-client';
   errorsLoaded$ = this.store.select(AppState.getErrors);
   subscriptions$: Subscription[] = [];
 
-  constructor(
-    private store: Store,
-    private toastService: ToastService,
-    private reasonService: ReasonService,
-    private errorService: ErrorService
-  ) {
+  constructor(private store: Store, private toastService: ToastService) {
     (window as any)['initMap'] = () => {
       store.dispatch(new MapLoaded());
     };
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions$.forEach((sub) => sub.unsubscribe());
   }
 
   ngOnInit(): void {
@@ -45,7 +40,6 @@ export class AppComponent implements OnInit {
     }
 
     this.mapLoad();
-    this.loadReasons();
   }
 
   mapLoad() {
@@ -55,24 +49,6 @@ export class AppComponent implements OnInit {
           this.toastService.showError(error);
         });
       }
-    });
-    this.subscriptions$.push(sub);
-  }
-
-  loadReasons() {
-    const filter = new FilterModel();
-    filter.pageSize = 100;
-    const sub = this.reasonService.getReasons(filter).subscribe({
-      next: (reasons) => {
-        this.store.dispatch(new LoadReasons(reasons.data));
-      },
-      error: (error) => {
-        processError(error.error || error, 'Error cargando motivos').forEach(
-          (err) => {
-            this.errorService.addGeneralError(err);
-          }
-        );
-      },
     });
     this.subscriptions$.push(sub);
   }
--- src/app/core/admin-template/admin-template.component.ts
+++ src/app/core/admin-template/admin-template.component.ts
@@ -1,12 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LayoutService } from '../service/layout.service';
+import { FilterModel } from '../models/filter.model';
+import { LoadReasons } from '../ng-xs-store/actions/app.actions';
+import { processError } from '../utils/error.utils';
+import { ReasonService } from '../../inventory/service/reason.service';
+import { Store } from '@ngxs/store';
+import { ErrorService } from '../service/error.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'gpa-admin-template',
   templateUrl: './admin-template.component.html',
   styleUrl: './admin-template.component.css',
 })
-export class AdminTemplateComponent implements OnInit {
-  constructor(public layoutService: LayoutService) {}
-  ngOnInit(): void {}
+export class AdminTemplateComponent implements OnInit, OnDestroy {
+  subscriptions$: Subscription[] = [];
+
+  constructor(
+    public layoutService: LayoutService,
+    public store: Store,
+    private reasonService: ReasonService,
+    private errorService: ErrorService
+  ) {}
+
+  ngOnDestroy(): void {
+    this.subscriptions$.forEach((sub) => sub.unsubscribe());
+  }
+
+  ngOnInit(): void {
+    this.loadReasons();
+  }
+
+  loadReasons() {
+    const filter = new FilterModel();
+    filter.pageSize = 100;
+    const sub = this.reasonService.getReasons(filter).subscribe({
+      next: (reasons) => {
+        this.store.dispatch(new LoadReasons(reasons.data));
+      },
+      error: (error) => {
+        processError(error.error || error, 'Error cargando motivos').forEach(
+          (err) => {
+            this.errorService.addGeneralError(err);
+          }
+        );
+      },
+    });
+    this.subscriptions$.push(sub);
+  }
 }
